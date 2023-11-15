75bf09505e726e40abadffa1d8b69b676f248edb
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-11-15 17:07:19 -0300

    fix removed python-dotenv

--- api/geocodingapi.py
+++ api/geocodingapi.py
@@ -8,6 +8,7 @@ class Geocoding:
         self.url = os.environ.get('GEOCODING_URL')
 
     def get_location(self, address):
+        return [self.key, self.url]
         res = requests.get(
             self.url,
             {
